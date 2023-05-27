f707aa5ddd83ce615a274671a5fbfc860eb89f7c
Serhii Horiaiev <shoriaiev@gmail.com>
2023-05-27 22:20:13 +0300

    Parsing process finished + gitignore added

--- parsing.py
+++ parsing.py
@@ -10,24 +10,25 @@ if __name__ == '__main__':
     # with open("film_table.html", "w") as f:
     #     f.write(html_lst[0])
 
-    # for page in html_lst:
-    page = html_lst[0]
-    soup = BeautifulSoup(page, 'html.parser')
-    film_elements = soup.find_all('li', class_='item')
-    for film in film_elements:
-        result_dict = {}
-        info_div = film.find('div', class_='info')
-        name = info_div.find('a', class_='name')
-        info = info_div.find('span')
-        # print(info.text)
+    for page in html_lst:
+        soup = BeautifulSoup(page, 'html.parser')
+        film_elements = soup.find_all('li', class_='item')
+        for film in film_elements:
+            result_dict = {}
+            info_div = film.find('div', class_='info')
+            name = info_div.find('a', class_='name')
+            info = info_div.find('span')
 
-        result_dict['ru_name'] = re.findall(r'^[^\n(]+', name.text)[0].strip()
-        result_dict['name'] = re.findall(r'^([^\s][^(]+)\s', info.text)[0] if re.match(r'^[^\s]', info.text) else None
-        result_dict['year'] = re.findall(r'\((\d{4})', info.text)[0]
+            result_dict['ru_name'] = re.findall(r'^\(*[^\n(]+', name.text)[0].strip()
+            result_dict['name'] = re.findall(r'^([^\s][^(]*)\s', info.text)[0] if re.match(r'^[^\s]', info.text) else None
+            result_dict['year'] = re.findall(r'\((\d{4})', info.text)[0]
 
-        if re.search(r'\(сериал\)', name.text):
-            res_shows.append(result_dict)
-        else:
-            res_films.append(result_dict)
+            if re.search(r'\(сериал\)', name.text):
+                res_shows.append(result_dict)
+            else:
+                res_films.append(result_dict)
 
-    print(res_films)
+    print(len(res_films))
+    print(len(res_shows))
+    print(res_films[0:10])
+    print(res_shows[0:10])
