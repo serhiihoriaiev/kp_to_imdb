734856e9443b1e122defcea29e89d79edb7c658a
Serhii Horiaiev <shoriaiev@gmail.com>
2023-11-10 19:26:57 +0200

    two more functions added to main

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import kp_functions
+import imdb_functions
 
 choices = {
     '1': kp_functions.load_watchlist,
-    '2': kp_functions.load_ratings
+    '2': kp_functions.load_ratings,
+    '3': imdb_functions.add_to_watchlist,
+    '4': imdb_functions.rate_item
 }
 
 if __name__ == '__main__':
 
-    choice = input("What do you want?\n\t1. KP watchlist\n\t2. KP ratings\n ")
+    choice = input("What do you want?\n\t1. Export KP watchlist\n\t2. Export KP ratings\n\t" 
+                   "3. Add movies/shows to IMDB watchlist\n\t4. Rate movies/shows on IMDB\n")
     choices[choice]()
